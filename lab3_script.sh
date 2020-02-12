842d6ff8a6bcf14ee97df8a4165952159a12722b
RHughes98 <ryan.hughes.md@gmail.com>
2020-02-12 12:44:17 -0700

    Adding all of the files for lab 3

--- lab3_script.sh
+++ lab3_script.sh
@@ -21,3 +21,5 @@ echo `grep -P '303-\d{3}-\d{4}' regex_practice.txt`
 echo "Writing geocities.com emails to file:"
 echo `grep -P '.+@geocities\.com' regex_practice.txt >> email_results.txt`
 echo "Done"
+git add "email_results.txt"
+git commit -m "added email results from script"
