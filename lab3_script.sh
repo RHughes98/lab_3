02d22345316b72089b98e2af000e80ca3c20c147
RHughes98 <ryan.hughes.md@gmail.com>
2020-02-12 12:33:26 -0700

    Completed lab3 script task 4

--- lab3_script.sh
+++ lab3_script.sh
@@ -14,3 +14,10 @@ echo "Matches:"
 echo `grep $reg $filename`
 echo "Number of (303) phone numbers:"
 echo `grep -P -c  '303-\d{3}-\d{4}' regex_practice.txt`
+echo "Number of emails:"
+echo `grep -P -c  '.+@.+\.\w{3}' regex_practice.txt`
+echo "List of (303) phone numbers:"
+echo `grep -P '303-\d{3}-\d{4}' regex_practice.txt`
+echo "Writing geocities.com emails to file:"
+echo `grep -P '.+@geocities\.com' regex_practice.txt >> email_results.txt`
+echo "Done"
